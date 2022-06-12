10a0b32894d3e540b27921abba3917cf16bd2ad9
Iva Milkovski <ivamilkovski44@gmail.com>
2022-06-12 12:24:37 +0200

    Add files via upload

---
+++ ProjekatASP.Implementation/Email/SmtpEmailSender.cs
@@ -0,0 +1,34 @@
+﻿using ProjekatASP.Application.DataTransfer.Email;
+using ProjekatASP.Application.Email;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatASP.Implementation.Email
+{
+	public class SmtpEmailSender : IEmailSender
+	{
+		public void SendEmail(SendEmailDto dto)
+		{
+			var smtp = new SmtpClient
+			{
+				Host = "smtp.gmail.com",
+				Port = 587,
+				EnableSsl = true,
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				UseDefaultCredentials = false,
+				Credentials = new NetworkCredential("aspblogmejl@gmail.com", "Sifra12345!")
+			};
+
+			var message = new MailMessage("aspblogmejl@gmail.com", dto.SendTo);
+			message.Subject = dto.Subject;
+			message.Body = dto.Content;
+			message.IsBodyHtml = true;
+			smtp.Send(message);
+		}
+	}
+}
